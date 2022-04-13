fd9f9d55f6dbb344bd5a2008a1074dc52c9a6c3a
natsuto-kun <natsuto391@gmail.com>
2022-04-13 23:04:02 +0900

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Luxiem-fun-site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
+
+export default config;
