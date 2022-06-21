cab220e1622ea9597ebc0ef9337f5da1f03862bd
yama-chiiii <nagayama0409@gmail.com>
2022-06-21 16:59:22 +0900

    add: ヘッダーにロゴを追加しました

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,11 +13,12 @@ const HeaderWrapper = styled.div`
   height: 100%;
 `
 
+
 export const Header = () => {
     return (
         <SiteHeader>
             <HeaderWrapper>
-                <p>header</p>
+                <img src = "https://raw.githubusercontent.com/natsuto-kun/Luxiem-fun-site/f57e66d816690ee58f561eabdfa0e35b6e6311f5/src/images/header-logo-black.svg"/>
             </HeaderWrapper>
         </SiteHeader>
     );
